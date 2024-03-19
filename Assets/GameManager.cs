526652d28555df999d398b26dd304505128e154a
Eli <elizatelkamp@gmail.com>
2024-03-19 11:32:15 -0400

    Added main menu

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,3 +1,10 @@
+//////////////////////////////////////////////
+//Assignment/Lab/Project: Collisions
+//Name: Eliza Majernik
+//Section: SGD.213.2172
+//Instructor: Brian Sowers
+//Date: 03/25/2024
+/////////////////////////////////////////////
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
