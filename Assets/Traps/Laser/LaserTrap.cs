a00645a9e19d0561cb32e9197badb5a118c35a28
Eli <elizatelkamp@gmail.com>
2024-03-25 20:59:13 -0400

    Added header

--- Assets/Traps/Laser/LaserTrap.cs
+++ Assets/Traps/Laser/LaserTrap.cs
@@ -1,3 +1,10 @@
+//////////////////////////////////////////////
+//Assignment/Lab/Project: Collisions
+//Name: Eliza Majernik
+//Section: SGD.213.2172
+//Instructor: Brian Sowers
+//Date: 03/25/2024
+/////////////////////////////////////////////
 using System;
 using System.Collections;
 using System.Collections.Generic;
